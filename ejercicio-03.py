7a22ba54b3619d2a3ac028bb0ed4436ed46a15f9
Pablo-rpg <81604980+Pablo-rpg@users.noreply.github.com>
2021-04-23 21:28:36 -0500

    pablo

---
+++ ejercicio-03.py
@@ -0,0 +1,16 @@
+#Operaciones con operadores de incremento
+
+a = 23
+print("a es :",a)
+a = a + 1
+print("a es :",a)
+a+=1
+print("a es :",a) 
+a*=2
+print("a es :",a)
+a/=2
+print("a es :",a)
+a-=2
+print("a es :",a)
+a**=2
+print("a es :",a)
